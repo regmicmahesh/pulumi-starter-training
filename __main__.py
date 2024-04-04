b82419284602eddc16d97916f91bd6d9ee2544c9
Regmi C. Mahesh <0xmahadev@gmail.com>
2024-04-04 22:17:46 +0545

    initial commit

---
+++ __main__.py
@@ -0,0 +1,40 @@
+import pulumi
+import pulumi_aws as aws
+
+size = "t2.micro"
+
+ami = aws.ec2.get_ami(
+    most_recent=True,
+    owners=["amazon"],
+    filters=[aws.ec2.GetAmiFilterArgs(name="name", values=["amzn2-ami-hvm-*"])],
+)
+
+group = aws.ec2.SecurityGroup(
+    "web-secgrp",
+    description="Enable HTTP access",
+    ingress=[
+        aws.ec2.SecurityGroupIngressArgs(
+            protocol="tcp",
+            from_port=80,
+            to_port=80,
+            cidr_blocks=["0.0.0.0/0"],
+        )
+    ],
+)
+
+user_data = """
+#!/bin/bash
+echo "Hello, World!" > index.html
+nohup python -m SimpleHTTPServer 80 &
+"""
+
+server = aws.ec2.Instance(
+    "web-server-www",
+    instance_type=size,
+    vpc_security_group_ids=[group.id],
+    user_data=user_data,
+    ami=ami.id,
+)
+
+pulumi.export("public_ip", server.public_ip)
+pulumi.export("public_dns", server.public_dns)
